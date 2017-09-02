c37a3afde18c1371d6956ec74dd3c60b9c182992
415ec080 <415ec080@gmail.com>
2017-09-03 00:48:49 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http=require('http');
 var fs=require('fs');
 var url=require('url');
 var  filename ='info.html';
-
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8081;
 
 http.createServer(function(req,res){
 res.writeHead(200,{'Content-Type':'text/html'});
@@ -15,6 +15,6 @@ console.log(req.url);
 
 
 
-}).listen(8081,'0.0.0.0');
+}).listen(port);
 
 console.log("running at 127.0.0.1:8081");
